c457aa4d52bb6693c60f60f4a3b512d20605e702
QW <adityajayakusuma@MacBook-Pro-aditya.local>
2025-04-24 13:42:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/belajarapi/Team.java
@@ -0,0 +1,19 @@
+package com.example.belajarapi;
+
+public class Team {
+    private String strTeam;
+    private String strStadium;
+
+    public String getStrTeam() {
+        return strTeam;
+    }
+    private String strBadge;
+
+    public String getStrBadge() {
+        return strBadge;
+    }
+
+    public String getStrStadium() {
+        return strStadium;
+    }
+}
